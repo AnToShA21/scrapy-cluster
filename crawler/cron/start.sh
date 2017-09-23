4ff18320581597388e4f9eec593cf4797a0557fd
antosha <antosha21es@gmail.com>
2017-09-23 21:42:31 +0300

    add cron

---
+++ crawler/cron/start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+cd /home/antosha/python/scrapy-cluster/crawler;
+
+if ! /bin/ps aux |/bin/grep -v grep |/bin/grep 'scrapy runspider' -q
+    then  scrapy runspider crawling/spiders/link_spider.py 2>&1 &
+fi
