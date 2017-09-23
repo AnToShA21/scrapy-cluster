3d2fc317de37d333a09dcb0f0886a6e5fc5cc60e
antosha <antosha21es@gmail.com>
2017-09-23 22:45:00 +0300

    add git pull to cron

--- crawler/cron/start.sh
+++ crawler/cron/start.sh
@@ -4,5 +4,7 @@
 cd /home/antosha/python/scrapy-cluster/crawler;
 
 if ! /bin/ps aux |/bin/grep -v grep |/bin/grep 'scrapy runspider' -q
-    then  /usr/bin/python /usr/local/bin/scrapy  runspider crawling/spiders/link_spider.py 2>&1 &
+    then
+        git pull
+        /usr/bin/python /usr/local/bin/scrapy  runspider crawling/spiders/link_spider.py 2>&1 &
 fi
